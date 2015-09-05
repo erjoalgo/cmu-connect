151aa239a3cd614f31a51572bbb6e2edb8fe3130
Ernesto Alfonso <erjoalgo@gmail.com>
2015-09-05 10:51:50 -0400

    initial

---
+++ cmuwpa-gen.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+read -p "andrew id: " ANDREWID 1>&2 
+stty -echo
+read -p "pass: " PASS 1>&2 ; echo 
+stty echo
+
+cat<<EOF
+network={
+	ssid="CMU-SECURE"
+	scan_ssid=1
+	key_mgmt=WPA-EAP
+	pairwise=CCMP TKIP
+	group=CCMP TKIP
+	eap=PEAP
+	identity="${ANDREWID}"
+	password="${PASS}"
+	ca_cert="cmu_comodo_cert.pem"
+	phase1="peapver=0"
+	phase2="MSCHAPV2"
+}
+EOF
